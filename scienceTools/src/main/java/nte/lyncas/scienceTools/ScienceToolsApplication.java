d2365ea19b820b5af0bec0834346ffdd2956d5bc
Renato Ganske <renato.j@lyncas.net>
2022-04-20 16:10:36 -0300

    Commit inicial

---
+++ scienceTools/src/main/java/nte/lyncas/scienceTools/ScienceToolsApplication.java
@@ -0,0 +1,13 @@
+package nte.lyncas.scienceTools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScienceToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScienceToolsApplication.class, args);
+	}
+
+}
---
+++ scienceTools/src/test/java/nte/lyncas/scienceTools/ScienceToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package nte.lyncas.scienceTools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScienceToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
